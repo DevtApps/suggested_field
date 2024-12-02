8b6fad3519841d8c305c7f2e0278857c0434e00a
Tiago Silva <devtappsinc@gmail.com>
2024-12-02 10:29:42 -0300

    added hint on SuggestedField

--- lib/suggested_field.dart
+++ lib/suggested_field.dart
@@ -4,33 +4,32 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:suggested_field/suggestion.dart';
 
-
 class SuggestedField extends StatelessWidget {
-   bool loading = false;
-   List<Suggestion> suggestions = [];
+  bool loading = false;
+  List<Suggestion> suggestions = [];
   Function(Suggestion) onTap;
- 
+  String hint;
+
   bool closeOnEmpty;
   FocusNode focusNode = FocusNode();
-
 
   RxList<Suggestion> filtered = RxList<Suggestion>();
 
   SuggestedField(
       {super.key,
+      this.hint = "Search...",
       this.closeOnEmpty = true,
       required this.suggestions,
       this.loading = false,
-      required this.onTap}){
-
-      focusNode.addListener(() { 
-        if(focusNode.hasFocus){
-          filter("");
-        }else{
-          filtered.clear();
-        }
-      });
+      required this.onTap}) {
+    focusNode.addListener(() {
+      if (focusNode.hasFocus) {
+        filter("");
+      } else {
+        filtered.clear();
       }
+    });
+  }
 
   void filter(String text) {
     if (text.isEmpty) {
@@ -55,9 +54,8 @@ class SuggestedField extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return WillPopScope(
-      onWillPop: ()async{
-
-        if(focusNode.hasFocus){
+      onWillPop: () async {
+        if (focusNode.hasFocus) {
           focusNode.unfocus();
           return false;
         }
@@ -65,56 +63,57 @@ class SuggestedField extends StatelessWidget {
         return true;
       },
       child: Material(
-      color: Colors.transparent,
-      child: Card(
-        clipBehavior: Clip.antiAlias,
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          TextField(
-           focusNode: focusNode,
-            onChanged: filter,
-            decoration: const InputDecoration(
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                hintStyle: TextStyle(color: Colors.grey),
-                border: InputBorder.none,
-                hintText: "Pesquise postos de recarga...",
-                suffixIcon: Icon(Icons.search)),
-          ),
-          Obx(
-            () => AnimatedContainer(
-              duration: const Duration(milliseconds: 500),
-              curve: Curves.ease,
-              height: loading
-                      ? 60
-                      : ((filtered.firstOrNull?.subtitle != null ? 60 : 50) *
-                              filtered.length)
-                          .toDouble(),
-              constraints: BoxConstraints(maxHeight: size.height / 3),
-              child: loading
-                  ? const Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : ListView(
-                      padding: EdgeInsets.zero,
-                      children: filtered
-                          .map((e) => ListTile(
-                                dense: true,
-                                title: Text(e.title),
-                                subtitle: e.subtitle != null
-                                    ? Text(e.subtitle!)
-                                    : null,
-                                onTap: () {
-                                   FocusScope.of(context).unfocus();
-                                  
-                                  onTap(e);
-                                },
-                              ))
-                          .toList(),
-                    ),
+        color: Colors.transparent,
+        child: Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            TextField(
+              focusNode: focusNode,
+              onChanged: filter,
+              decoration: InputDecoration(
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  border: InputBorder.none,
+                  hintText: hint,
+                  suffixIcon: const Icon(Icons.search)),
             ),
-          ),
-        ]),
-      ),),
+            Obx(
+              () => AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.ease,
+                height: loading
+                    ? 60
+                    : ((filtered.firstOrNull?.subtitle != null ? 60 : 50) *
+                            filtered.length)
+                        .toDouble(),
+                constraints: BoxConstraints(maxHeight: size.height / 3),
+                child: loading
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : ListView(
+                        padding: EdgeInsets.zero,
+                        children: filtered
+                            .map((e) => ListTile(
+                                  dense: true,
+                                  title: Text(e.title),
+                                  subtitle: e.subtitle != null
+                                      ? Text(e.subtitle!)
+                                      : null,
+                                  onTap: () {
+                                    FocusScope.of(context).unfocus();
+
+                                    onTap(e);
+                                  },
+                                ))
+                            .toList(),
+                      ),
+              ),
+            ),
+          ]),
+        ),
+      ),
     );
   }
 }
